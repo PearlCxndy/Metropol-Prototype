9b5c082fa68323d08197689ea869048a85ea14d3
Rizq Yazed <rizqyazed@MacBookPro.lan>
2025-09-22 11:34:28 +0100

    added daily,weekly and monthly event endpoints

--- Metropol-BACKEND/src/modules/event/event.controller.ts
+++ Metropol-BACKEND/src/modules/event/event.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import prisma from "../../utils/connect";
 
-// Get All of the Users
+// Get Event by ID
 export const getEventbyId = async (req: Request, res: Response) => {
   try {
     const eventId = parseInt(req.params.id);
-    console.log(eventId);
     const event = await prisma.event.findUnique({
       where: {
         event_id: eventId,
@@ -21,8 +20,72 @@ export const getEventbyId = async (req: Request, res: Response) => {
 export const getEvents = async (req: Request, res: Response) => {
   try {
     const events = await prisma.event.findMany({});
-    console.log(events);
     res.status(200).json({ data: events });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+};
+
+export const getDailyEvents = async (req: Request, res: Response) => {
+  try {
+    const today = new Date(
+      new Date().toISOString().split("T")[0] + "T00:00:00.000Z"
+    );
+    const event = await prisma.event.findMany({
+      where: {
+        event_time: {
+          equals: today,
+        },
+      },
+    });
+    res.status(200).json({ data: event });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+};
+
+export const getWeeklyEvents = async (req: Request, res: Response) => {
+  try {
+    const today = new Date(
+      new Date().toISOString().split("T")[0] + "T00:00:00.000Z"
+    );
+    const nextWeek = new Date(today);
+    nextWeek.setDate(nextWeek.getDate() + 7);
+    nextWeek.toISOString();
+    const event = await prisma.event.findMany({
+      where: {
+        event_time: {
+          lte: nextWeek,
+          gte: today,
+        },
+      },
+    });
+    res.status(200).json({ data: event });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+};
+
+export const getMonthlyEvents = async (req: Request, res: Response) => {
+  try {
+    const today = new Date(
+      new Date().toISOString().split("T")[0] + "T00:00:00.000Z"
+    );
+    const nextMonth = new Date(today);
+    nextMonth.setDate(nextMonth.getDate() + 30);
+    nextMonth.toISOString();
+    const event = await prisma.event.findMany({
+      where: {
+        event_time: {
+          lte: nextMonth,
+          gte: today,
+        },
+      },
+    });
+    res.status(200).json({ data: event });
   } catch (e) {
     console.log(e);
     res.status(500);
--- Metropol-BACKEND/src/modules/event/event.router.ts
+++ Metropol-BACKEND/src/modules/event/event.router.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { getEventbyId, getEvents } from "./event.controller";
+import {
+  getDailyEvents,
+  getEventbyId,
+  getEvents,
+  getMonthlyEvents,
+  getWeeklyEvents,
+} from "./event.controller";
 import passport from "passport";
 
 const eventRouter = Router();
-eventRouter.get("", getEvents);
+
+eventRouter.get("/daily", getDailyEvents);
+eventRouter.get("/weekly", getWeeklyEvents);
+eventRouter.get("/monthly", getMonthlyEvents);
 eventRouter.get("/:id", getEventbyId);
+eventRouter.get("", getEvents);
 
 export default eventRouter;
